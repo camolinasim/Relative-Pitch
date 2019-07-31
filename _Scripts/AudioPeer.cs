d5f162f0b6c10add82f7a68f30d769fde912769b
Cristian Molina <cmutep@gmail.com>
2019-07-31 01:28:28 -0600

    Start of Relative Pitch

---
+++ _Scripts/AudioPeer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioPeer : MonoBehaviour {
+    AudioSource _audioSource;
+    public float[] _samples = new float[512];
+    public int counter = 0;
+    // Start is called before the first frame update
+    void Start() {
+        _audioSource = GetComponent<AudioSource> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource ();
+        _audioSource.Play();
+    }
+
+    void GetSpectrumAudioSource(){
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+}
